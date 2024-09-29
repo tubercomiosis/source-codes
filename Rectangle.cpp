7b5e3925870ce74189f75d2b31f0a90a945a405c
Tubercomi <gseleniumepic5@gmail.com>
2024-09-29 16:44:52 +0200

    Create Rectangle.cpp

---
+++ Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+
+int main() {
+	HDC desk;
+	int sw, sh;
+	HBRUSH hbr;
+	while (true) {
+		desk = GetDC(NULL);
+		sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+		hbr = CreateSolidBrush(RGB(rand () % 255, rand () % 255, rand () % 255));
+		SelectObject(desk, hbr);
+		Rectangle(desk, rand () % sw, rand () % sh, rand () % sw, rand () % sh);
+		DeleteObject(hbr);
+		ReleaseDC(NULL, desk);
+		Sleep(10);
+	}
+}
