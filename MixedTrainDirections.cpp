750e2533bf31612e055a28469906bf3882403818
Tubercomi <gseleniumepic5@gmail.com>
2024-10-02 19:06:55 +0200

    Create MixedTrainDirections.cpp

---
+++ MixedTrainDirections.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+
+int main() {
+	HDC hdc;
+	int sw, sh;
+	while (1) {
+		hdc = GetDC(NULL);
+		sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+		BitBlt(hdc, 0, -30, sw, sh, hdc, 0, 0, SRCCOPY);
+		BitBlt(hdc, 0, sh - 30, sw, sh, hdc, 0, 0, SRCCOPY);
+		BitBlt(hdc, -30, 0, sw, sh, hdc, 0, 0, SRCCOPY);
+		BitBlt(hdc, sw - 30, 0, sw, sh, hdc, 0, 0, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		Sleep(10);
+	}
+}
