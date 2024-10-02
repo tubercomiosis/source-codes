90538fd9993987932d9d709ed14213d7c2e3a529
Tubercomi <gseleniumepic5@gmail.com>
2024-10-02 19:11:00 +0200

    Create Brightening.cpp

---
+++ Brightening.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+int main() {
+	HDC hdc;
+	int sw, sh;
+	while (1) {
+		hdc = GetDC(NULL);
+		sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+		BitBlt(hdc, rand () % 2, rand () % 2, sw, sh, hdc, rand () % 2, rand () % 2, SRCPAINT);
+		ReleaseDC(NULL, hdc);
+		Sleep(10);
+	}
+}
