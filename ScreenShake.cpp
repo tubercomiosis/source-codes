5ccfd2757df5343114499312c4f7298d69c90dc7
Tubercomi <gseleniumepic5@gmail.com>
2024-10-02 15:32:13 +0200

    Create ScreenShake.cpp

---
+++ ScreenShake.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+int main() {
+	HDC hdc;
+	int sw, sh;
+	while (1) {
+		hdc = GetDC(NULL);
+		sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+		BitBlt(hdc, rand () % 5, rand () % 5, sw, sh, hdc, rand () % 5, rand () % 5, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		Sleep(10);
+	}
+}
