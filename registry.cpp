3ae83f6b39613587e007acbbdbf3dff10ffc4f63
tubercomi man <gseleniumepic5@gmail.com>
2024-10-15 18:26:20 +0200

    Update registry.cpp

--- registry.cpp
+++ registry.cpp
@@ -1,10 +1,10 @@
-void RegAddKeyA(HKEY key, LPCSTR subkey, LPCSTR value, ULONG type, ULONG value) {
+void RegAddKeyA(HKEY key, LPCSTR subkey, LPCSTR name, ULONG type, UINT value) {
 	HKEY hkey;
 	DWORD disp;
 	long result;
 	
 	result = RegCreateKeyExA(key, subkey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hkey, &disp);
-	result = RegSetValueExA(key, value, 0, type, (const BYTE*)&value, (int)sizeof(value));
+	result = RegSetValueExA(key, name, 0, type, (const BYTE*)&value, (int)sizeof(value));
 	
 	RegCloseKey(hkey);
 }
