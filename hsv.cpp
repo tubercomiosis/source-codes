f72cc04dd9005423190e991904abde0d4a58714e
tubercomi man <gseleniumepic5@gmail.com>
2024-10-28 19:43:22 +0100

    Create hsv.cpp

---
+++ hsv.cpp
@@ -0,0 +1,32 @@
+COLORREF crSpectrum(int crLength) { // I rewrote this from raduminecraft
+    double H = fmod(crLength, 360);
+    double S = 1.0;
+    double V = 0.5;
+    double C = (1.0-fabs(2.0*V-1.0)) * S;
+    double X = C*(1.0-fabs(fmod(H/60.0, 2.0) - 1.0));
+    double M = V - C / 2.0;
+    double R, G, B;
+    if (H < 60) {
+        R = C; G = X, B = 0;
+    }
+    else if (H < 120) {
+        R = X; G = C; B = 0;
+    }
+    else if (H < 180) {
+        R = 0; G = C; B = X;
+    }
+    else if (H < 240) {
+        R = 0; G = X; B = C;
+    }
+    else if (H < 300) {
+        R = X; G = 0; B = C;
+    }
+    else {
+        R = C; G = 0; B = X;
+    }
+    
+    int crRed = static_cast<INT>((R + M) * 255);
+    int crGreen = static_cast<INT>((G + M) * 255);
+    int crBlue = static_cast<INT>((B + M) * 255);
+    return RGB(crRed, crGreen, crBlue);
+}
