2765e4f3b685f77b0b77b67d81f1b953f08e2faf
NotCCR <gseleniumepic5@gmail.com>
2024-11-08 16:58:40 +0100

    Update registry.cpp

--- registry.cpp
+++ registry.cpp
@@ -1,11 +1,13 @@
-VOID regAdd(HKEY hk, LPCWSTR lpsub, LPCWSTR lpval, DWORD dwtype, UINT val) {
-	HKEY hky;
-	DWORD dwd;
-	long res;
-	
-	res = RegCreateKeyExW(hk, lpsub, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hk, &dwd);
-	res = RegSetValueExW(hk, lpval, 0, dwtype, (const BYTE*)&val, (int)sizeof(val));
-	
-	RegCloseKey(hky);
-	return;
+VOID WINAPI RegAddW(HKEY hKey, LPCWSTR lpRegistryKey, LPCWSTR lpRegName, DWORD dwRegType, LPBYTE bValue, DWORD dwSize) {
+    HKEY hKResult;
+    RegCreateKeyW(hKey, lpRegistryKey, &hKResult);
+    RegSetValueExW(hKResult, lpRegName, 0, dwRegType, bValue, dwSize);
+    RegCloseKey(hKResult);
+}
+
+VOID WINAPI RegSetW(HKEY hKey, LPCWSTR lpRegistryKey, LPCWSTR lpRegName, DWORD dwRegType, LPBYTE bValue, DWORD dwSize) {
+    HKEY hKResult;
+    RegOpenKeyExW(hKey, lpRegistryKey, 0, KEY_SET_VALUE, &hKResult);
+    RegSetValueExW(hKResult, lpRegName, 0, dwRegType, bValue, dwSize);
+    RegCloseKey(hKResult);
 }
