519966aa9c6cc637e2537df4ac459ad4f04c868a
NotCCR <gseleniumepic5@gmail.com>
2024-12-09 16:05:16 +0100

    Update registry.cpp

--- registry.cpp
+++ registry.cpp
@@ -1,6 +1,23 @@
-LONG WINAPI RegAddW(HKEY hKey, LPCWSTR lpRegistryKey, LPCWSTR lpRegName, DWORD dwRegType, BYTE bValue) {
-    HKEY hKResult;
-    RegCreateKeyW(hKey, lpRegistryKey, &hKResult);
-    RegSetValueExW(hKResult, lpRegName, 0, dwRegType, &bValue, sizeof(bValue));
-    RegCloseKey(hKResult);
+FLONG WINAPI ManageRegW(HKEY hKey, LPCWSTR lpSubKey, LPCWSTR lpValueName, DWORD dwType, BYTE lpData, LPBYTE lpData2, bool isSet, bool isString) {
+    HKEY hkResult;
+    if (isSet == false) {
+        if (isString == false) {
+            RegCreateKeyW(hKey, lpSubKey, &hkResult);
+            RegSetValueExW(hkResult, lpValueName, 0, dwType, &lpData, sizeof(lpData));
+        }
+        else if (isString == true) {
+            RegCreateKeyW(hKey, lpSubKey, &hkResult);
+            RegSetValueExW(hkResult, lpValueName, 0, REG_SZ, lpData2, sizeof(lpData2) * 16);
+        }
+    }
+    else if (isSet == true) {
+        if (isString == false) {
+            RegOpenKeyW(hKey, lpSubKey, &hkResult);
+            RegSetValueExW(hkResult, lpValueName, 0, dwType, &lpData, sizeof(lpData));
+        }
+        else if (isString == true) {
+            RegOpenKeyW(hKey, lpSubKey, &hkResult);
+            RegSetValueExW(hkResult, lpValueName, 0, REG_SZ, lpData2, sizeof(lpData2) * 16);
+        }
+    }
 }
